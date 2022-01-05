a5a2ca3b3836282b51f447bdfd6e1d973f85dbb9
Roger Mendez <roger61087@gmail.com>
2022-01-05 15:56:44 -0500

    commit tree 10dba7dbc98feecbcec863edf6b97c9c2cdc0bcf

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'red', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 3, 'M', 'red', 'angry', true);
+
+
